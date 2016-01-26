845b7333264ffa80422ff061ab93f34acd7fb184
BlubbFish <git@blubbfish.net>
2016-01-26 14:20:42 +0000

    First Version Ever that runs a little bit

--- FileLogger.cs
+++ FileLogger.cs
@@ -4,44 +4,38 @@ using System.Linq;
 using System.Text;
 using System.IO;
 
-namespace BlubbFish.Utils
-{
-    public class FileLogger
-    {
-        private static Dictionary<string, FileLogger> instances = new Dictionary<string, FileLogger>();
-        private StreamWriter file;
-        private FileLogger(string filename, bool append)
-        {
-            if (!File.Exists(filename))
-            {
-                string folder = Path.GetDirectoryName(Path.GetFullPath(filename));
-                if (!Directory.Exists(folder))
-                {
-                    Directory.CreateDirectory(folder);
-                }
-            }
-            this.file = new StreamWriter(filename, append, Encoding.UTF8);
-            this.file.AutoFlush = true;
+namespace BlubbFish.Utils {
+  public class FileLogger {
+    private static Dictionary<string, FileLogger> instances = new Dictionary<string, FileLogger>();
+    private StreamWriter file;
+    private FileLogger(string filename, bool append) {
+      if(!File.Exists(filename)) {
+        string folder = Path.GetDirectoryName(Path.GetFullPath(filename));
+        if(!Directory.Exists(folder)) {
+          Directory.CreateDirectory(folder);
         }
-        public static FileLogger getInstance(string filename, bool append)
-        {
-            if (!instances.Keys.Contains(filename))
-            {
-                instances.Add(filename, new FileLogger(filename, append));
-            }
-            return instances[filename];
-        }
-
-        public void setArray(string[] text)
-        {
-            this.file.Write(String.Join(file.NewLine, text) + file.NewLine);
-            this.file.Flush();
-        }
-
-        public void setLine(string text)
-        {
-            this.file.WriteLine(text);
-            this.file.Flush();
-        }
+      }
+      this.file = new StreamWriter(filename, append, Encoding.UTF8);
+      this.file.AutoFlush = true;
     }
+    public static FileLogger getInstance(string filename, bool append) {
+      if(!instances.Keys.Contains(filename)) {
+        instances.Add(filename, new FileLogger(filename, append));
+      }
+      return instances[filename];
+    }
+
+    public void setArray(string[] text) {
+      this.file.Write(String.Join(file.NewLine, text) + file.NewLine);
+      this.file.Flush();
+    }
+
+    public void setLine(string text) {
+      this.file.WriteLine(text);
+      this.file.Flush();
+    }
+    public void setLine(string text, DateTime d) {
+      this.setLine(d.ToString("[yyyy-MM-dd HH:mm:ss.ffff] " + text));
+    }
+  }
 }
