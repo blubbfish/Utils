22d8a9ff2afcf8337618420d52359b372397de36
BlubbFish <git@blubbfish.net>
2018-09-06 20:26:38 +0000

    Cleanups

--- OwnModel.cs
+++ OwnModel.cs
@@ -9,7 +9,7 @@ namespace BlubbFish.Utils
   public abstract class OwnModel<T> where T : class
   {
     private static readonly Lazy<T> _instance = new Lazy<T>(() => CreateInstanceOfT());
-    private List<OwnView> observer = new List<OwnView>();
+    private readonly List<OwnView> observer = new List<OwnView>();
     public static T Instance
     {
       get {
@@ -35,5 +35,6 @@ namespace BlubbFish.Utils
       this.observer.ForEach(delegate (OwnView view) { view.Update(); });
     }
     abstract protected void Init();
+    abstract public void Dispose();
   }
 }
